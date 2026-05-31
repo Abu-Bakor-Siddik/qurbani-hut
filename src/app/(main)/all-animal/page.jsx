97d6ee94687dac2fcdac626558b5dc0996f410c8
Abu Bakor Siddik (Shuvo) <141806136+Abs-shuvo@users.noreply.github.com>
2026-05-31 20:41:49 +0600

    initializing the whole structure/skelton of the webapp

---
+++ src/app/(main)/all-animal/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllAnimalPage = () => {
+    return (
+        <div>
+            <h2>All Animal Page here</h2>
+        </div>
+    );
+};
+
+export default AllAnimalPage;
---
+++ src/app/(main)/animal-details/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AnimalDetailsPage = () => {
+    return (
+        <div>
+            <h2>This is for animal details Page</h2>
+        </div>
+    );
+};
+
+export default AnimalDetailsPage;
